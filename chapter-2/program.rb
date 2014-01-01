c66607c943d5b3f8ed32925a5ff36aaf84de2df9
Marcus Rugger <marcusrugger@yahoo.com>
2014-01-01 09:43:41 -0500

    Add Boolean, If, and LessThan

--- chapter-2/program.rb
+++ chapter-2/program.rb
@@ -1,12 +1,17 @@
 require "./machine"
 require "./number"
+require "./boolean"
 require "./variable"
+require "./less_than"
 require "./add"
 require "./multiply"
+require "./do_nothing"
 require "./assign"
+require "./if"
 
 environment = { x: Number.new(2), y: Number.new(3) }
 
+=begin
 Machine.new(
   Assign.new(
     :x,
@@ -16,4 +21,14 @@ Machine.new(
       )
     ),
   environment
+  ).run
+=end
+
+Machine.new(
+  If.new(
+    Variable.new(:x),
+    Assign.new(:y, Number.new(1)),
+    Assign.new(:y, Number.new(2))
+    ),
+  { x: Boolean.new(false) }
   ).run
